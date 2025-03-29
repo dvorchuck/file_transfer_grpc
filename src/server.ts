bad3552d694a677a66a2f67599613b8e0d81a60f
dvorchuck <daniel.dvorak@gmail.com>
2025-03-29 16:09:48 +0100

    fix: missing case where clientId is not checked

--- src/server.ts
+++ src/server.ts
@@ -8,13 +8,26 @@ const STORAGE_PATH_SERVER = path.resolve("storage-server");
 
 const server: FileServiceServer = {
   syncFiles: async (call) => {
-    console.log("Received file sync request for:", call.metadata);
-    console.log("Received file sync request for:", call.request.info);
-
-    const storage = path.resolve(
-      STORAGE_PATH_SERVER,
-      call.metadata.get("clientId")[0] as string
+    console.log(
+      `Received file sync request for metadata: ${JSON.stringify(
+        call.metadata
+      )} and payload: ${JSON.stringify(call.request.info)}`
     );
+
+    const clientName = call.metadata.get("clientId")[0];
+    // use of metadata:
+    // https://grpc.io/docs/guides/metadata/
+    // "gRPC metadata is a key-value pair of data that is sent with initial or final gRPC requests or responses. It is used to provide additional information about the call, such as authentication credentials, tracing information, or custom headers."
+
+    if (!clientName || typeof clientName !== "string") {
+      console.error(
+        `Expected clientId as a stirng in metadata. Received: ${clientName.toString()}`
+      );
+      call.end();
+      return;
+    }
+
+    const storage = path.resolve(STORAGE_PATH_SERVER, clientName);
 
     const fileDataList = await gatherFileData(storage);
 
