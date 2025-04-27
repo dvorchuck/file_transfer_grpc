6bd55f12262e1208026427f5f17cae52d159ecbc
dvorchuck <daniel.dvorak@gmail.com>
2025-04-27 14:23:52 +0200

    fix: gathering file hashes is not parallel

--- src/file.ts
+++ src/file.ts
@@ -1,40 +1,48 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as crypto from "crypto";
+import { fsService } from "./read.service";
 
 export async function gatherFileData(
-  dirPath: string,
-  basePath: string = dirPath
-): Promise<{ path: string; hash: string }[]> {
-  const fileHashes: { path: string; hash: string }[] = [];
+  dirPath: string
+): Promise<Map<string, string>> {
+  const startTime = performance.now();
+  /** key: path, value: hash */
+  const fileHashes = new Map<string, string>();
 
-  // Get all files and directories in the current directory
-  const filesAndDirs = await fs.promises.readdir(dirPath, {
-    withFileTypes: true,
-  });
+  async function recursion(dirPath: string, basePath: string = dirPath) {
+    const filesAndDirs = await fs.promises.readdir(dirPath, {
+      withFileTypes: true,
+    });
 
-  // Iterate over each item
-  for (const fileOrDir of filesAndDirs) {
-    const fullPath = path.join(dirPath, fileOrDir.name);
-    const relativePath = path.relative(basePath, fullPath);
+    return Promise.all(
+      filesAndDirs.map(async (fileOrDir) => {
+        // Iterate over each item
+        const fullPath = path.join(dirPath, fileOrDir.name);
+        const relativePath = path.relative(basePath, fullPath);
 
-    if (fileOrDir.isDirectory()) {
-      // If it's a directory, recursively process it
-      const nestedHashes = await gatherFileData(fullPath, basePath);
-      fileHashes.push(...nestedHashes);
-    } else if (fileOrDir.isFile()) {
-      // If it's a file, calculate its hash
-      const hash = await calculateFileHash(fullPath);
-      fileHashes.push({ path: relativePath, hash });
-    }
+        if (fileOrDir.isDirectory()) {
+          // If it's a directory, recursively process it
+          return recursion(fullPath, basePath);
+        } else if (fileOrDir.isFile()) {
+          // If it's a file, calculate its hash
+          const hash = await calculateFileHash(fullPath);
+          fileHashes.set(relativePath, hash);
+        }
+      })
+    );
   }
 
+  await recursion(dirPath);
+  // Get all files and directories in the current directory
+
+  console.log(`gatherFileData took: ${performance.now() - startTime}ms`);
   return fileHashes;
 }
 
 export async function calculateFileHash(filePath: string): Promise<string> {
   const hash = crypto.createHash("sha256");
-  const fileStream = fs.createReadStream(filePath);
+  const fileStream = await fsService.getReadStream(filePath);
 
   return new Promise((resolve, reject) => {
     fileStream.on("data", (data) => {
@@ -46,6 +54,7 @@ export async function calculateFileHash(filePath: string): Promise<string> {
     });
 
     fileStream.on("error", (err) => {
+      console.error(err);
       reject(err);
     });
   });
